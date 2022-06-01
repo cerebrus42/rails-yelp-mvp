7f868832a46dbac043b845e2639d393b2e51caee
Kai Goedecke <kai@cerebrus.de>
2022-06-01 13:22:45 +0200

    Add code

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'cleanup'
+Restaurant.destroy_all
+
+5.times do
+  puts 'create Restaurant'
+  name = Faker::Restaurant.name
+  address = Faker::Address.full_address
+  phone_number = Faker::PhoneNumber.phone_number
+  category = %w[chinese italian japanese french belgian].sample
+  puts "Name: #{name}"
+  puts "Address: #{address}"
+  puts "Phone Number: #{phone_number}"
+  puts "Category: #{category}"
+  new_restaurant = Restaurant.new(name: name, address: address, phone_number: phone_number, category: category)
+  new_restaurant.save
+end
